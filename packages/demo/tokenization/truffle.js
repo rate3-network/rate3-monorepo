7589178bb2f3e8919e1b7d8f876473effe5ecae4
Wu Di <7753104+justwudi@users.noreply.github.com>
2018-08-27 10:54:42 +0800

    Add contracts for tokenization demo

---
+++ packages/demo/tokenization/truffle.js
@@ -0,0 +1,30 @@
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
+ */
+
+/* eslint-disable import/no-extraneous-dependencies */
+require('babel-register');
+require('babel-polyfill');
+/* eslint-enable import/no-extraneous-dependencies */
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
+    },
+  },
+};
