33fb87c8216f799ce70da69b83cd17153c9c1ac6
Wu Di <7753104+justwudi@users.noreply.github.com>
2018-09-04 10:16:01 +0800

    Use existing root path as router basename

--- packages/demo/tokenization/src/index.js
+++ packages/demo/tokenization/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route, BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import { I18nextProvider } from 'react-i18next';
+
 import App from './pages/App';
 import store from './Store';
 import './index.css';
 import TranslationHandler from './translations';
 
+const history = createBrowserHistory();
 const i18next = TranslationHandler.init();
 
 // Using BrowserRouter instead of HashRouter for hashed routing because
@@ -16,7 +19,7 @@ const i18next = TranslationHandler.init();
 ReactDOM.render(
   <I18nextProvider i18n={i18next}>
     <Provider store={store}>
-      <BrowserRouter basename="#">
+      <BrowserRouter basename={`${history.location.pathname.substring(1)}#`}>
         <Route component={App} />
       </BrowserRouter>
     </Provider>
