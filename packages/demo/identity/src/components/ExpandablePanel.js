69e3d3b98a26d89b31cfba02bcc3827ca5c5484f
LookRain <brantscube@hotmail.com>
2018-09-10 01:38:16 +0800

    text styles

--- packages/demo/identity/src/components/ExpandablePanel.js
+++ packages/demo/identity/src/components/ExpandablePanel.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import classNames from 'classnames';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
+
+import identityIcon from '../assets/identityIcon.svg';
 
 const styles = theme => ({
   root: {
@@ -32,8 +32,23 @@ const styles = theme => ({
   details: {
     alignItems: 'center',
   },
-  column: {
-    flexBasis: '33.33%',
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '50%',
+    // flexBasis: '33.33%',
+  },
+  image: {
+    width: '4.5em',
+  },
+  textGroup: {
+    height: '60%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    padding: '1em 0 1em 1em',
+    // alignItems: 'center',
   },
   helper: {
     borderLeft: `2px solid ${theme.palette.divider}`,
@@ -46,6 +61,14 @@ const styles = theme => ({
       textDecoration: 'underline',
     },
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: '1em',
+  },
+  verificationStatus: {
+    // color: '',
+    fontWeight: 'bold',
+  }
 });
 
 const ArrowIcon = withStyles(styles)((props) => {
@@ -59,11 +82,14 @@ const DetailedExpansionPanel = (props) => {
     <div className={classes.root}>
       <ExpansionPanel>
         <ExpansionPanelSummary expandIcon={<ArrowIcon />}>
-          <div className={classes.column}>
-            <Typography className={classes.heading}>Location</Typography>
-            <Typography className={classes.heading}>1 Verification</Typography>
+          <div className={classes.header}>
+            <img src={identityIcon} className={classes.image} alt="Identity Icon" />
+            <div className={classes.textGroup}>
+              <Typography className={classes.title}>Name</Typography>
+              <Typography className={classes.verificationStatus}>1 Verification</Typography>
+            </div>
           </div>
-          <div className={classes.column}>
+          <div className={classes.header}>
             <Typography className={classes.secondaryHeading}>Select trip destination</Typography>
           </div>
         </ExpansionPanelSummary>
