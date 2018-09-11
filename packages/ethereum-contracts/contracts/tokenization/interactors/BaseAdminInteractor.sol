5165597fc5aa66566d2d53a086f4f7077ae7e903
Waihon Chee <imwaihon@gmail.com>
2018-09-11 13:21:17 +0800

    Create dual admin operations/admin flow

--- packages/ethereum-contracts/contracts/tokenization/interactors/BaseAdminInteractor.sol
+++ packages/ethereum-contracts/contracts/tokenization/interactors/BaseAdminInteractor.sol
@@ -8,10 +8,27 @@ contract BaseAdminInteractor is Claimable {
     using SafeMath for uint256;
 
     TokenizeTemplateToken public token;
-    address public admin;
+    address public admin1;
+    address public admin2;
+
+    modifier onlyAdmin1() {
+        require(msg.sender == admin1, "Only allowed for Admin1");
+        _;
+    }
+
+    modifier onlyAdmin2() {
+        require(msg.sender == admin2, "Only allowed for Admin2");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin1 || msg.sender == admin2, "Only allowed for Admins");
+        _;
+    }
 
     constructor(TokenizeTemplateToken _token) {
-        admin = msg.sender;
+        admin1 = msg.sender;
+        admin2 = msg.sender;
         token = _token;
     }
 
@@ -19,9 +36,14 @@ contract BaseAdminInteractor is Claimable {
         token = _newTokenContract;
     }
 
-    function setAdmin(address _newAdminAddress) public onlyOwner {
+    function setFirstAdmin(address _newAdminAddress) public onlyOwner {
         require(_newAdminAddress != address(0), "Admin cannot be 0x0 address");
-        admin = _newAdminAddress;
+        admin1 = _newAdminAddress;
+    }
+
+    function setSecondAdmin(address _newAdminAddress) public onlyOwner {
+        require(_newAdminAddress != address(0), "Admin cannot be 0x0 address");
+        admin2 = _newAdminAddress;
     }
 
     function claimTokenOwnership(TokenizeTemplateToken _newTokenContract) public onlyOwner {
