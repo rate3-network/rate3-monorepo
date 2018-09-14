9bad220d1aa2700392c65c06f0558c709363ef53
Waihon Chee <imwaihon@gmail.com>
2018-09-14 17:07:01 +0800

    Added specific registry interactor actions

--- packages/ethereum-contracts/contracts/tokenization/interactors/RegistryInteractor.sol
+++ packages/ethereum-contracts/contracts/tokenization/interactors/RegistryInteractor.sol
@@ -6,12 +6,65 @@ import "./BaseAdminInteractor.sol";
 contract RegistryInteractor is BaseAdminInteractor {
     using SafeMath for uint256;
 
+    string public constant WHITELISTED_FOR_MINT = "WHITELISTED_FOR_MINT";
+    string public constant WHITELISTED_FOR_BURN = "WHITELISTED_FOR_BURN";
+    string public constant WHITELISTED_FOR_TRANSFER = "WHITELISTED_FOR_TRANSFER";
+    string public constant BLACKLISTED = "BLACKLISTED";
+
     constructor(
         TokenizeTemplateToken _token
     ) 
         public
         BaseAdminInteractor(_token)
     {
+    }
+
+    function whitelistForMint(address _address) public onlyAdmin {
+        token.setKeyDataRecord(
+            _address,
+            WHITELISTED_FOR_MINT,
+            0,
+            "",
+            address(0),
+            true,
+            msg.sender
+        );
+    }
+
+    function whitelistForBurn(address _address) public onlyAdmin {
+        token.setKeyDataRecord(
+            _address,
+            WHITELISTED_FOR_BURN,
+            0,
+            "",
+            address(0),
+            true,
+            msg.sender
+        );
+    }
+
+    function whitelistFoTransfer(address _address) public onlyAdmin {
+        token.setKeyDataRecord(
+            _address,
+            WHITELISTED_FOR_TRANSFER,
+            0,
+            "",
+            address(0),
+            true,
+            msg.sender
+        );
+    }
+
+    function blacklist(address _address) public onlyAdmin {
+        token.setKeyDataRecord(
+            _address,
+            BLACKLISTED,
+            0,
+            "",
+            address(0),
+            true,
+            msg.sender
+        );
     }
 
     function setKeyDataRecord(
