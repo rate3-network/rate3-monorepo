383664ac1353fa91e055e3a5b7c98875f1bd26da
Waihon Chee <imwaihon@gmail.com>
2018-09-18 15:14:38 +0800

    Burn operations test

--- packages/ethereum-contracts/test/tokenization/OperationsInteractorTest.js
+++ packages/ethereum-contracts/test/tokenization/OperationsInteractorTest.js
@@ -45,6 +45,7 @@ contract('OperationsInteractor Tests', function(accounts) {
             await this.interactor.setSecondAdmin(admin2, { from: owner });
 
             await this.interactor.whitelistForMint(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForMint(rest[1], true, { from: admin2 });
         });
 
         it('anyone can submit as many mint requests', async function() {
@@ -89,6 +90,7 @@ contract('OperationsInteractor Tests', function(accounts) {
             await this.interactor.setSecondAdmin(admin2, { from: owner });
 
             await this.interactor.whitelistForMint(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForMint(rest[1], true, { from: admin2 });
         });
 
         it('owner cannot approve mint request', async function() {
@@ -132,6 +134,7 @@ contract('OperationsInteractor Tests', function(accounts) {
             await this.interactor.setSecondAdmin(admin2, { from: owner });
 
             await this.interactor.whitelistForMint(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForMint(rest[1], true, { from: admin2 });
         });
 
         it('owner cannot finalize mint request', async function() {
@@ -152,6 +155,169 @@ contract('OperationsInteractor Tests', function(accounts) {
             await this.interactor.finalizeMint(rest[0], 0, { from: admin1 }).should.be.rejected;
             await this.interactor.finalizeMint(rest[0], 0, { from: rest[0] }).should.be.rejected;
             await this.interactor.finalizeMint(rest[0], 0, { from: rest[1] }).should.be.rejected;
+        });
+    });
+
+    describe('Test - burn request operations', function() {
+        beforeEach(async function() {
+            this.token = await TokenizeTemplateToken.new({ from: owner });
+            this.interactor = await TokenizeTemplateInteractor.new(this.token.address, { from: owner });
+
+            this.balanceModule = await BalanceModule.new({ from: owner });
+            this.allowanceModule = await AllowanceModule.new({ from: owner });
+            this.registryModule = await RegistryModule.new({ from: owner });
+            await this.balanceModule.transferOwnership(this.token.address, { from: owner });
+            await this.allowanceModule.transferOwnership(this.token.address, { from: owner });
+            await this.registryModule.transferOwnership(this.token.address, { from: owner });
+            await this.token.setAllowanceModule(this.allowanceModule.address);
+            await this.token.setBalanceModule(this.balanceModule.address);
+            await this.token.setRegistryModule(this.registryModule.address);
+
+            await this.token.transferOwnership(this.interactor.address, { from: owner });
+            await this.interactor.setToken(this.token.address, { from: owner });
+            await this.interactor.claimTokenOwnership({ from: owner });
+
+            await this.interactor.setFirstAdmin(admin1, { from: owner });
+            await this.interactor.setSecondAdmin(admin2, { from: owner });
+
+            await this.interactor.whitelistForMint(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForMint(rest[1], true, { from: admin2 });
+            await this.interactor.whitelistForBurn(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForBurn(rest[1], true, { from: admin2 });
+
+            await this.interactor.requestMint(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveMint(rest[0], 0, { from: admin1 });
+            await this.interactor.finalizeMint(rest[0], 0, { from: admin2 });
+            await this.interactor.requestMint(new web3.BigNumber('10000e+18'), { from: rest[1] });
+            await this.interactor.approveMint(rest[1], 0, { from: admin1 });
+            await this.interactor.finalizeMint(rest[1], 0, { from: admin2 });
+        });
+
+        it('anyone can submit as many burn requests', async function() {
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] }).should.be.fulfilled;
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[1] }).should.be.fulfilled;
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[1] }).should.be.fulfilled;
+        });
+
+        it('burn requests are indexed', async function() {
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+
+            await this.interactor.approveBurn(rest[0], 0, { from: admin1 }).should.be.fulfilled;
+            await this.interactor.approveBurn(rest[0], 1, { from: admin1 }).should.be.fulfilled;
+            await this.interactor.approveBurn(rest[0], 2, { from: admin1 }).should.be.fulfilled;
+            await this.interactor.approveBurn(rest[0], new web3.BigNumber('3'), { from: admin1 }).should.be.fulfilled;
+        });
+    });
+
+    describe('Test - burn approve operations', function() {
+        beforeEach(async function() {
+            this.token = await TokenizeTemplateToken.new({ from: owner });
+            this.interactor = await TokenizeTemplateInteractor.new(this.token.address, { from: owner });
+
+            this.balanceModule = await BalanceModule.new({ from: owner });
+            this.allowanceModule = await AllowanceModule.new({ from: owner });
+            this.registryModule = await RegistryModule.new({ from: owner });
+            await this.balanceModule.transferOwnership(this.token.address, { from: owner });
+            await this.allowanceModule.transferOwnership(this.token.address, { from: owner });
+            await this.registryModule.transferOwnership(this.token.address, { from: owner });
+            await this.token.setAllowanceModule(this.allowanceModule.address);
+            await this.token.setBalanceModule(this.balanceModule.address);
+            await this.token.setRegistryModule(this.registryModule.address);
+
+            await this.token.transferOwnership(this.interactor.address, { from: owner });
+            await this.interactor.setToken(this.token.address, { from: owner });
+            await this.interactor.claimTokenOwnership({ from: owner });
+
+            await this.interactor.setFirstAdmin(admin1, { from: owner });
+            await this.interactor.setSecondAdmin(admin2, { from: owner });
+
+            await this.interactor.whitelistForMint(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForMint(rest[1], true, { from: admin2 });
+            await this.interactor.whitelistForBurn(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForBurn(rest[1], true, { from: admin2 });
+
+            await this.interactor.requestMint(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveMint(rest[0], 0, { from: admin1 });
+            await this.interactor.finalizeMint(rest[0], 0, { from: admin2 });
+            await this.interactor.requestMint(new web3.BigNumber('10000e+18'), { from: rest[1] });
+            await this.interactor.approveMint(rest[1], 0, { from: admin1 });
+            await this.interactor.finalizeMint(rest[1], 0, { from: admin2 });
+        });
+
+        it('owner cannot approve burn request', async function() {
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveBurn(rest[0], 0, { from: owner }).should.be.rejected;
+        });
+
+        it('admin1 can approve burn request', async function() {
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveBurn(rest[0], 0, { from: admin1 }).should.be.fulfilled;
+        });
+
+        it('non-owner/admin2 cannot approve burn request', async function() {
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveBurn(rest[0], 0, { from: admin2 }).should.be.rejected;
+            await this.interactor.approveBurn(rest[0], 0, { from: rest[0] }).should.be.rejected;
+            await this.interactor.approveBurn(rest[0], 0, { from: rest[1] }).should.be.rejected;
+        });
+    });
+
+    describe('Test - burn finalize operations', function() {
+        beforeEach(async function() {
+            this.token = await TokenizeTemplateToken.new({ from: owner });
+            this.interactor = await TokenizeTemplateInteractor.new(this.token.address, { from: owner });
+
+            this.balanceModule = await BalanceModule.new({ from: owner });
+            this.allowanceModule = await AllowanceModule.new({ from: owner });
+            this.registryModule = await RegistryModule.new({ from: owner });
+            await this.balanceModule.transferOwnership(this.token.address, { from: owner });
+            await this.allowanceModule.transferOwnership(this.token.address, { from: owner });
+            await this.registryModule.transferOwnership(this.token.address, { from: owner });
+            await this.token.setAllowanceModule(this.allowanceModule.address);
+            await this.token.setBalanceModule(this.balanceModule.address);
+            await this.token.setRegistryModule(this.registryModule.address);
+
+            await this.token.transferOwnership(this.interactor.address, { from: owner });
+            await this.interactor.setToken(this.token.address, { from: owner });
+            await this.interactor.claimTokenOwnership({ from: owner });
+
+            await this.interactor.setFirstAdmin(admin1, { from: owner });
+            await this.interactor.setSecondAdmin(admin2, { from: owner });
+
+            await this.interactor.whitelistForMint(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForMint(rest[1], true, { from: admin2 });
+            await this.interactor.whitelistForBurn(rest[0], true, { from: admin2 });
+            await this.interactor.whitelistForBurn(rest[1], true, { from: admin2 });
+
+            await this.interactor.requestMint(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveMint(rest[0], 0, { from: admin1 });
+            await this.interactor.finalizeMint(rest[0], 0, { from: admin2 });
+            await this.interactor.requestMint(new web3.BigNumber('10000e+18'), { from: rest[1] });
+            await this.interactor.approveMint(rest[1], 0, { from: admin1 });
+            await this.interactor.finalizeMint(rest[1], 0, { from: admin2 });
+        });
+
+        it('owner cannot finalize burn request', async function() {
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveBurn(rest[0], 0, { from: admin1 });
+            await this.interactor.finalizeBurn(rest[0], 0, { from: owner }).should.be.rejected;
+        });
+
+        it('admin2 can finalize burn request', async function() {
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveBurn(rest[0], 0, { from: admin1 });
+            await this.interactor.finalizeBurn(rest[0], 0, { from: admin2 }).should.be.fulfilled;
+        });
+
+        it('non-owner/admin1 cannot finalize mint request', async function() {
+            await this.interactor.requestBurn(new web3.BigNumber('10000e+18'), { from: rest[0] });
+            await this.interactor.approveBurn(rest[0], 0, { from: admin1 });
+            await this.interactor.finalizeBurn(rest[0], 0, { from: admin1 }).should.be.rejected;
+            await this.interactor.finalizeBurn(rest[0], 0, { from: rest[0] }).should.be.rejected;
+            await this.interactor.finalizeBurn(rest[0], 0, { from: rest[1] }).should.be.rejected;
         });
     });
 });
