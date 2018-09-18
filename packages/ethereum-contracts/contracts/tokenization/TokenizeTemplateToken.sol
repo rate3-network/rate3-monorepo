42af9ca2002c6db77a81bbf7a18022b36d37bb83
Waihon Chee <imwaihon@gmail.com>
2018-09-18 17:55:57 +0800

    Add it to TTT

--- packages/ethereum-contracts/contracts/tokenization/TokenizeTemplateToken.sol
+++ packages/ethereum-contracts/contracts/tokenization/TokenizeTemplateToken.sol
@@ -2,9 +2,10 @@ pragma solidity ^0.4.24;
 
 import "../lib/math/SafeMath.sol";
 import "./layers/ComplianceLayer.sol";
+import "./layers/DelegateLayer.sol";
 import "./modules/ModularToken.sol";
 
-contract TokenizeTemplateToken is ComplianceLayer {
+contract TokenizeTemplateToken is ComplianceLayer, DelegateLayer {
     using SafeMath for uint256;
 
     string public name = "Tokenized Template Token";
