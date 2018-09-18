14b11f66c9a929455b15b6cff3090dc0746ab8f4
Waihon Chee <imwaihon@gmail.com>
2018-09-18 18:12:24 +0800

    Add DelegateInterface

--- packages/ethereum-contracts/contracts/tokenization/layers/DelegateLayer.sol
+++ packages/ethereum-contracts/contracts/tokenization/layers/DelegateLayer.sol
@@ -2,10 +2,21 @@ pragma solidity ^0.4.24;
 
 import "../modules/ModularToken.sol";
 
+interface DelegateInterface {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256 balance);
+    function allowance(address _owner, address _spender) external view returns (uint256);
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
+    function increaseApproval(address _spender, uint _addedValue) external returns (bool);
+    function decreaseApproval(address _spender, uint _subtractedValue) external returns (bool);
+}
+
 contract DelegateLayer is ModularToken {
-    DelegateLayer public delegate;
+    DelegateInterface public delegate;
     
-    function setDelegate(DelegateLayer _delegateAddress) public onlyOwner {
+    function setDelegate(DelegateInterface _delegateAddress) public onlyOwner {
         delegate = _delegateAddress;
     }
 
@@ -128,5 +139,4 @@ contract DelegateLayer is ModularToken {
             return delegate.decreaseApproval(_spender, _subtractedValue);
         }
     }
-
 }
