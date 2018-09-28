ae8237e7acc10846853e008629736dad9d6610f8
Waihon Chee <imwaihon@gmail.com>
2018-09-28 15:01:47 +0800

    Add OperationsInteractor documentation

--- packages/ethereum-contracts/contracts/tokenization/interactors/OperationsInteractor.sol
+++ packages/ethereum-contracts/contracts/tokenization/interactors/OperationsInteractor.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.24;
 
-import "../../lib/math/SafeMath.sol";
 import "../shared/TokenInterface.sol";
 import "./AdminInteractor.sol";
 
@@ -10,8 +9,13 @@ import "./AdminInteractor.sol";
  * @notice Operations flow: Request -> Approval by first admin -> Finalize by second admin
  */
 contract OperationsInteractor is AdminInteractor {
-    using SafeMath for uint256;
-
+    /**
+     * @notice MintRequestOperation keeps track of the state of a requested
+     * mint, and updated accordingly during approval and finalization stages.
+     *
+     * @dev Since minimum mint amount > 0, a revoked MintRequestOperation is
+     * identified by value == 0.
+     */
     struct MintRequestOperation {
         address by;
         uint256 value;
@@ -21,6 +25,13 @@ contract OperationsInteractor is AdminInteractor {
         uint256 approvalTimestamp;
     }
 
+    /**
+     * @notice BurnRequestOperation keeps track of the state of a requested
+     * burn, and updated accordingly during approval and finalization stages.
+     *
+     * @dev Since minimum burn amount > 0, a revoked BurnRequestOperation is
+     * identified by value == 0.
+     */
     struct BurnRequestOperation {
         address by;
         uint256 value;
@@ -30,26 +41,42 @@ contract OperationsInteractor is AdminInteractor {
         uint256 approvalTimestamp;
     }
 
+    /// @notice Each address has an array of MintRequestOperations in order.
     mapping (address => MintRequestOperation[]) public mintRequestOperations;
+
+    /// @notice Each address has an array of BurnRequestOperations in order.
     mapping (address => BurnRequestOperation[]) public burnRequestOperations;
 
+    /// @notice Pause all operations except Owner functionalities and revoke.
     bool operationsPaused;
 
+    /// @notice Only when operations are not paused.
     modifier operationsNotPaused() {
         require(!operationsPaused, "Operations are paused");
         _;
     }
 
+    /**
+     * @notice Check if address is cleared for minting.
+     *
+     * @param _mintDestinationAddress Destination address to mint tokens to.
+     */
     modifier whitelistedForMint(address _mintDestinationAddress) {
         require(TokenInterface(token).isWhitelistedForMint(_mintDestinationAddress), "Not whitelisted for mint");
         _;
     }
 
+    /**
+     * @notice Check if address is cleared for burning.
+     *
+     * @param _burnOriginAddress Destination address to burn tokens from.
+     */
     modifier whitelistedForBurn(address _burnOriginAddress) {
         require(TokenInterface(token).isWhitelistedForBurn(_burnOriginAddress), "Not whitelisted for burn");
         _;
     }
-    
+
+    /// Events
     event MintOperationRequested(address indexed by, uint256 value, uint256 requestTimestamp, uint256 index);
     event BurnOperationRequested(address indexed by, uint256 value, uint256 requestTimestamp, uint256 index);
 
@@ -62,6 +89,11 @@ contract OperationsInteractor is AdminInteractor {
     event MintOperationRevoked(address indexed by, address indexed revokedBy, uint256 revokedTimestamp, uint256 index);
     event BurnOperationRevoked(address indexed by, address indexed revokedBy, uint256 revokedTimestamp, uint256 index);
 
+    /**
+     * @notice Request mint, fires off a MintOperationRequested event.
+     *
+     * @param _value Amount of tokens to mint.
+     */
     function requestMint(uint256 _value) public operationsNotPaused whitelistedForMint(msg.sender) {
         require(_value > 0, "Mint value should be more than 0");
 
@@ -72,6 +104,14 @@ contract OperationsInteractor is AdminInteractor {
         mintRequestOperations[msg.sender].push(mintRequestOperation);
     }
 
+    /**
+     * @notice Approve mint, fires off a MintOperationApproved event.
+     *
+     * @dev Can only be approved by Admin 1.
+     *
+     * @param _requestor Requestor of MintRequestOperation.
+     * @param _index Index of MintRequestOperation by _requestor.
+     */
     function approveMint(address _requestor, uint256 _index) public onlyAdmin1 operationsNotPaused whitelistedForMint(_requestor) {
         MintRequestOperation storage mintRequestOperation = mintRequestOperations[_requestor][_index];
 
@@ -85,6 +125,16 @@ contract OperationsInteractor is AdminInteractor {
         emit MintOperationApproved(_requestor, msg.sender, block.timestamp, _index);
     }
 
+    /**
+     * @notice Finalize mint, fires off a MintOperationFinalized event. Tokens
+     * will be minted.
+     *
+     * @dev Can only be approved by Admin 2. MintRequestOperation should be
+     * already approved beforehand.
+     *
+     * @param _requestor Requestor of MintRequestOperation.
+     * @param _index Index of MintRequestOperation by _requestor.
+     */
     function finalizeMint(address _requestor, uint256 _index) public onlyAdmin2 operationsNotPaused whitelistedForMint(_requestor) {
         MintRequestOperation memory mintRequestOperation = mintRequestOperations[_requestor][_index];
 
@@ -99,11 +149,24 @@ contract OperationsInteractor is AdminInteractor {
         emit MintOperationFinalized(_requestor, msg.sender, block.timestamp, _index);
     }
 
+    /**
+     * @notice Revokes a specific MintRequest.
+     *
+     * @dev Can only be approved by Admin 1 or Admin 2.
+     *
+     * @param _requestor Requestor of MintRequestOperation.
+     * @param _index Index of MintRequestOperation by _requestor.
+     */
     function revokeMint(address _requestor, uint256 _index) public onlyAdmin {
         delete mintRequestOperations[_requestor][_index];
         emit MintOperationRevoked(_requestor, msg.sender, block.timestamp, _index);
     }
 
+    /**
+     * @notice Request burn, fires off a BurnOperationRequested event.
+     *
+     * @param _value Number of tokens to burn.
+     */
     function requestBurn(uint256 _value) public operationsNotPaused whitelistedForBurn(msg.sender) {
         require(_value > 0, "Burn value should be more than 0");
 
@@ -114,6 +177,14 @@ contract OperationsInteractor is AdminInteractor {
         burnRequestOperations[msg.sender].push(burnRequestOperation);
     }
 
+    /**
+     * @notice Approve burn, fires off a BurnOperationApproved event.
+     *
+     * @dev Can only be approved by Admin 1.
+     *
+     * @param _requestor Requestor of BurnRequestOperation.
+     * @param _index Index of BurnRequestOperation by _requestor.
+     */
     function approveBurn(address _requestor, uint256 _index) public onlyAdmin1 operationsNotPaused whitelistedForBurn(_requestor) {
         BurnRequestOperation storage burnRequestOperation = burnRequestOperations[_requestor][_index];
 
@@ -127,6 +198,16 @@ contract OperationsInteractor is AdminInteractor {
         emit BurnOperationApproved(_requestor, msg.sender, block.timestamp, _index);
     }
 
+    /**
+     * @notice Finalize burn, fires off a BurnOperationFinalized event. Tokens
+     * will be burned.
+     *
+     * @dev Can only be approved by Admin 2. BurnRequestOperation should be
+     * already approved beforehand.
+     *
+     * @param _requestor Requestor of BurnRequestOperation.
+     * @param _index Index of BurnRequestOperation by _requestor.
+     */
     function finalizeBurn(address _requestor, uint256 _index) public onlyAdmin2 operationsNotPaused whitelistedForBurn(_requestor) {
         BurnRequestOperation memory burnRequestOperation = burnRequestOperations[_requestor][_index];
 
@@ -141,27 +222,50 @@ contract OperationsInteractor is AdminInteractor {
         emit BurnOperationFinalized(_requestor, msg.sender, block.timestamp, _index);
     }
 
+    /**
+     * @notice Revokes a specific BurnRequest.
+     *
+     * @dev Can only be approved by Admin 1 or Admin 2.
+     *
+     * @param _requestor Requestor of BurnRequestOperation.
+     * @param _index Index of BurnRequestOperation by _requestor.
+     */
     function revokeBurn(address _requestor, uint256 _index) public onlyAdmin {
         delete burnRequestOperations[_requestor][_index];
         emit BurnOperationRevoked(_requestor, msg.sender, block.timestamp, _index);
     }
 
-    function pauseOperations() public onlyOwner {
-        operationsPaused = true;
-    }
-
-    function unpauseOperations() public onlyOwner {
-        operationsPaused = false;
-    }
-
+    /**
+     * @notice Sweep a set amount of tokens from a particular address and send
+     * them to a holding account.
+     *
+     * @dev Useful when retrieving lost tokens or confiscation of tokens due to
+     * suspicious activities. Only Owner of interactor is allowed to do this.
+     *
+     * @param _from Account to sweep tokens from.
+     * @param _to Account to hold tokens on.
+     * @param _value Amount of tokens to sweep.
+     */
     function sweepToken(address _from, address _to, uint256 _value) public onlyOwner {
         TokenInterface(token).sweep(msg.sender, _from, _to, _value);
     }
 
+    /// @notice Pause operations for this interactor.
+    function pauseOperations() public onlyOwner {
+        operationsPaused = true;
+    }
+
+    /// @notice Resume operations for this interactor.
+    function unpauseOperations() public onlyOwner {
+        operationsPaused = false;
+    }
+
+    /// @notice Globally pause token ERC20 functionality.
     function pauseToken() public onlyOwner {
         TokenInterface(token).pause();
     }
 
+    /// @notice Globally resume token ERC20 functionality.
     function unpauseToken() public onlyOwner {
         TokenInterface(token).unpause();
     }
