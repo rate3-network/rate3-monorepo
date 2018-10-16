13280ad5c5d9452b3c96a38821e28c36bb4810ed
Waihon Chee <imwaihon@gmail.com>
2018-10-16 12:24:15 +0800

    New layout js stellar contracts

---
+++ packages/stellar-contracts/js/stellar.js
@@ -0,0 +1,8 @@
+import Stellar from 'stellar-sdk';
+
+const passPhrase = Stellar.Networks[process.env.STELLAR_NETWORK];
+Stellar.Network.use(new Stellar.Network(passPhrase));
+
+const stellar = new Stellar.Server(process.env.STELLAR_HORIZON_URI);
+
+export { stellar, Stellar };
