485db9864880c1cfb671aa0eadf8fd3d0dafefe5
Waihon Chee <imwaihon@gmail.com>
2018-10-17 16:38:09 +0800

    Finish conversion tests

--- packages/ethereum-contracts/test/bridge/ConversionReceiver.test.js
+++ packages/ethereum-contracts/test/bridge/ConversionReceiver.test.js
@@ -129,4 +129,80 @@ contract('ConversionReceiver Tests', function(accounts) {
             amount3.should.be.bignumber.equal(new web3.BigNumber('1000e+18'));
         });
     });
+
+    describe('Test - reject conversion', function() {
+        beforeEach(async function () {
+            await this.token.approve(this.receiver.address, new web3.BigNumber('1500e+18'), { from: alice });
+            await this.receiver.requestConversion(new web3.BigNumber('500e+18'), addrToBytes32(STELLAR_ADDRESS), { from: alice });
+            await this.receiver.requestConversion(new web3.BigNumber('500e+18'), addrToBytes32(STELLAR_ADDRESS), { from: alice });
+        });
+
+        it('only owner can reject conversion', async function() {
+            await assertRevert(this.receiver.rejectConversion(0, { from: alice }));
+            await this.receiver.rejectConversion(0, { from: owner });
+        });
+
+        it('only can reject open conversion', async function() {
+            await this.receiver.rejectConversion(0, { from: owner });
+            await assertRevert(this.receiver.rejectConversion(0, { from: owner }));
+
+            await this.receiver.acceptConversion(1, { from: owner });
+            await assertRevert(this.receiver.rejectConversion(1, { from: owner }));
+        });
+
+        it('reject conversion event emitted', async function() {
+            let { logs } = await this.receiver.rejectConversion(0, { from: owner });
+
+            const event1 = expectEvent.inLogs(logs, 'ConversionRejected', {
+                ethAddress: alice,
+                stellarAddress: addrToBytes32(STELLAR_ADDRESS),
+            });
+        })
+
+        it('tokens should be refunded in rejection', async function() {
+            let amount1 = await this.token.balanceOf(alice);
+            await this.receiver.rejectConversion(0, { from: owner });
+
+            let amount2 = await this.token.balanceOf(alice);
+            await this.receiver.rejectConversion(1, { from: owner });
+
+            let amount3 = await this.token.balanceOf(alice);
+
+            amount1.should.be.bignumber.equal(new web3.BigNumber(0));
+            amount2.should.be.bignumber.equal(new web3.BigNumber('500e+18'));
+            amount3.should.be.bignumber.equal(new web3.BigNumber('1000e+18'));
+        });
+    });
+
+    describe('Test - accept conversion', function() {
+        beforeEach(async function () {
+            await this.token.approve(this.receiver.address, new web3.BigNumber('1500e+18'), { from: alice });
+            await this.receiver.requestConversion(new web3.BigNumber('500e+18'), addrToBytes32(STELLAR_ADDRESS), { from: alice });
+            await this.receiver.requestConversion(new web3.BigNumber('500e+18'), addrToBytes32(STELLAR_ADDRESS), { from: alice });
+        });
+
+        it('only owner can accept conversion', async function() {
+            await assertRevert(this.receiver.acceptConversion(0, { from: alice }));
+            await this.receiver.acceptConversion(0, { from: owner });
+        });
+
+        it('only can accept open conversion', async function() {
+            await this.receiver.rejectConversion(0, { from: owner });
+            await assertRevert(this.receiver.acceptConversion(0, { from: owner }));
+
+            await this.receiver.acceptConversion(1, { from: owner });
+            await assertRevert(this.receiver.acceptConversion(1, { from: owner }));
+        });
+
+        it('accept conversion event emitted', async function() {
+            let { logs } = await this.receiver.acceptConversion(0, { from: owner });
+
+            const event1 = expectEvent.inLogs(logs, 'ConversionAccepted', {
+                ethAddress: alice,
+                stellarAddress: addrToBytes32(STELLAR_ADDRESS),
+            });
+        })
+
+        // Tokens should be burnt on accept (?)
+    });
 });
