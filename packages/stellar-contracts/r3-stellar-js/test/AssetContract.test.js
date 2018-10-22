7fc2fe9cd51f606091d3e896e30d1a6f0d986f2c
Waihon Chee <imwaihon@gmail.com>
2018-10-22 18:40:40 +0800

    Use chai-as-promised

--- packages/stellar-contracts/r3-stellar-js/test/AssetContract.test.js
+++ packages/stellar-contracts/r3-stellar-js/test/AssetContract.test.js
@@ -7,7 +7,6 @@ import axios from 'axios';
 require('chai')
 .use(require('chai-as-promised'))
 .should();
-const expect = require('chai').expect;
 
 describe("server.js tests", function () {
     beforeEach(async function () {
@@ -23,11 +22,7 @@ describe("server.js tests", function () {
 
     describe('Setup r3-stellar.js', function () {
         it("throws error for insecure server", async function () {
-            try {
-                await R3Stellar('http://horizon-live.stellar.org:1337')
-            } catch (err) {
-                expect(err.message).equal('Cannot connect to insecure horizon server');
-            }
+            await R3Stellar('http://horizon-live.stellar.org:1337').should.be.rejectedWith('Cannot connect to insecure horizon server');
         });
     });
 
