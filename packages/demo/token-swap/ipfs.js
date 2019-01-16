54ebb562df0b7516265668fec63a2e6847e43c95
Davis Gay <davis12529@users.noreply.github.com>
2019-01-16 21:10:08 +0800

    remove unnecessary commented lines

--- packages/demo/token-swap/ipfs.js
+++ packages/demo/token-swap/ipfs.js
@@ -2,15 +2,10 @@ const ipfsAPI = require('ipfs-api');
 const fs = require('fs');
 const path = require('path');
 
-// const ipfs = ipfsAPI('/dns4/36b3e177.ngrok.io', { protocol: 'http' });
-// const ipfs = ipfsAPI({ host: 'b77a785f.ngrok.io', port: '80', protocol: 'http' });
+
 
 const ipfs = ipfsAPI('/ip4/192.168.0.204/tcp/5001');
 // const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
-
-// const ipfs = ipfsAPI('http://192.168.0.204/tcp/5001');
-// const ipfs = ipfsAPI('http://52.15.62.13/tcp/80');
-// const ipfs = ipfsAPI('36b3e177.ngrok.io');
 
 // Reading file from folder
 const dir = 'build';
