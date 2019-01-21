b5433b8229a80c5ab1ca3e6618990a8cda11b8db
Waihon Chee <imwaihon@gmail.com>
2019-01-21 18:59:01 +0800

    Port AdminInteractor tests

--- packages/ethereum-contracts/test/tokenization/AdminInteractor.test.js
+++ packages/ethereum-contracts/test/tokenization/AdminInteractor.test.js
@@ -1,7 +1,4 @@
-import { increaseTimeTo, duration } from '../helpers/increaseTime';
-import latestTime from '../helpers/latestTime';
-import { advanceBlock } from '../helpers/advanceToBlock';
-import expectEvent from '../helpers/expectEvent';
+import { BN, constants, expectEvent, time, shouldFail } from 'openzeppelin-test-helpers';
 
 const AdminInteractor = artifacts.require("./tokenization/interactors/AdminInteractor.sol");
 const BaseProxy = artifacts.require("./tokenization/BaseProxy.sol");
@@ -12,14 +9,14 @@ const RegistryModule = artifacts.require("./tokenization/modules/RegistryModule.
 
 require('chai')
   .use(require('chai-as-promised'))
-  .use(require('chai-bignumber')(web3.BigNumber))
+  .use(require('chai-bn')(BN))
   .should();
 
 contract('AdminInteractor Tests', function(accounts) {
 
     before(async function () {
         // Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
-        await advanceBlock();
+        await time.advanceBlock();
     });
     /**
      * Roles
@@ -36,14 +33,14 @@ contract('AdminInteractor Tests', function(accounts) {
 
         it('only owner can set admin', async function() {
             // Non-admins cannot set themselves to be admin.
-            await this.interactor.setFirstAdmin(admin1, { from: admin1 }).should.be.rejected;
-            await this.interactor.setFirstAdmin(admin1, { from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.setFirstAdmin(admin1, { from: admin1 }));
+            await this.interactor.setFirstAdmin(admin1, { from: owner });
 
-            await this.interactor.setSecondAdmin(admin2, { from: admin2 }).should.be.rejected;
-            await this.interactor.setSecondAdmin(admin2, { from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.setSecondAdmin(admin2, { from: admin2 }));
+            await this.interactor.setSecondAdmin(admin2, { from: owner });
 
             // Existing admins cannot set admins either.
-            await this.interactor.setSecondAdmin(rest[0], { from: admin2 }).should.be.rejected;
+            await shouldFail.reverting(this.interactor.setSecondAdmin(rest[0], { from: admin2 }));
         });
 
         it('set admin actually works', async function() {
@@ -64,7 +61,7 @@ contract('AdminInteractor Tests', function(accounts) {
         });
 
         it('set first admin event emitted', async function() {
-            const { logs } = await this.interactor.setFirstAdmin(admin1, { from: owner }).should.be.fulfilled;
+            const { logs } = await this.interactor.setFirstAdmin(admin1, { from: owner });
 
             const event1 = expectEvent.inLogs(logs, 'FirstAdminSet', {
                 from: owner,
@@ -95,9 +92,9 @@ contract('AdminInteractor Tests', function(accounts) {
         });
 
         it('only owner can set token contract', async function() {
-            await this.interactor.setToken(this.token.address, { from: admin1 }).should.be.rejected;
-            await this.interactor.setToken(this.token.address, { from: admin2 }).should.be.rejected;
-            await this.interactor.setToken(this.token.address, { from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.setToken(this.token.address, { from: admin1 }));
+            await shouldFail.reverting(this.interactor.setToken(this.token.address, { from: admin2 }));
+            await this.interactor.setToken(this.token.address, { from: owner });
         });
 
         it('set token contract actually works', async function() {
@@ -113,17 +110,17 @@ contract('AdminInteractor Tests', function(accounts) {
         it('only owner can claim token ownership', async function() {
             await this.token.transferOwnership(this.interactor.address, { from: owner });
             await this.interactor.setToken(this.token.address, { from: owner });
-            await this.interactor.claimTokenOwnership({ from: admin1 }).should.be.rejected;
-            await this.interactor.claimTokenOwnership({ from: admin2 }).should.be.rejected;
-            await this.interactor.claimTokenOwnership({ from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.claimTokenOwnership({ from: admin1 }));
+            await shouldFail.reverting(this.interactor.claimTokenOwnership({ from: admin2 }));
+            await this.interactor.claimTokenOwnership({ from: owner });
         });
 
         it('can only claim token ownership after the ownership actually transferred', async function() {
             await this.interactor.setToken(this.token.address, { from: owner });
-            await this.interactor.claimTokenOwnership({ from: owner }).should.be.rejected;
+            await shouldFail.reverting(this.interactor.claimTokenOwnership({ from: owner }));
 
             await this.token.transferOwnership(this.interactor.address, { from: owner });
-            await this.interactor.claimTokenOwnership({ from: owner }).should.be.fulfilled;
+            await this.interactor.claimTokenOwnership({ from: owner });
         });
 
         it('set token contract event emitted', async function() {
@@ -149,9 +146,9 @@ contract('AdminInteractor Tests', function(accounts) {
         });
 
         it('only owner can set proxy contract', async function() {
-            await this.interactor.setProxy(this.token.address, { from: admin1 }).should.be.rejected;
-            await this.interactor.setProxy(this.token.address, { from: admin2 }).should.be.rejected;
-            await this.interactor.setProxy(this.token.address, { from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.setProxy(this.token.address, { from: admin1 }));
+            await shouldFail.reverting(this.interactor.setProxy(this.token.address, { from: admin2 }));
+            await this.interactor.setProxy(this.token.address, { from: owner });
         });
 
         it('set proxy contract actually works', async function() {
@@ -167,17 +164,17 @@ contract('AdminInteractor Tests', function(accounts) {
         it('only owner can claim proxy ownership', async function() {
             await this.proxy.transferOwnership(this.interactor.address, { from: owner });
             await this.interactor.setProxy(this.proxy.address, { from: owner });
-            await this.interactor.claimProxyOwnership({ from: admin1 }).should.be.rejected;
-            await this.interactor.claimProxyOwnership({ from: admin2 }).should.be.rejected;
-            await this.interactor.claimProxyOwnership({ from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.claimProxyOwnership({ from: admin1 }));
+            await shouldFail.reverting(this.interactor.claimProxyOwnership({ from: admin2 }));
+            await this.interactor.claimProxyOwnership({ from: owner });
         });
 
         it('can only claim proxy ownership after the ownership actually transferred', async function() {
             await this.interactor.setProxy(this.proxy.address, { from: owner });
-            await this.interactor.claimProxyOwnership({ from: owner }).should.be.rejected;
+            await shouldFail.reverting(this.interactor.claimProxyOwnership({ from: owner }));
 
             await this.proxy.transferOwnership(this.interactor.address, { from: owner });
-            await this.interactor.claimProxyOwnership({ from: owner }).should.be.fulfilled;
+            await this.interactor.claimProxyOwnership({ from: owner });
         });
 
         it('set proxy contract event emitted', async function() {
@@ -208,18 +205,18 @@ contract('AdminInteractor Tests', function(accounts) {
 
         it('token should be owned by interactor', async function() {
             await this.token.transferOwnership(this.interactor.address, { from: owner });
-            await this.interactor.setTokenOnProxy(this.token.address, { from: owner }).should.be.rejected;
+            await shouldFail.reverting(this.interactor.setTokenOnProxy(this.token.address, { from: owner }));
 
             await this.interactor.claimTokenOwnership({ from: owner });
-            await this.interactor.setTokenOnProxy(this.token.address, { from: owner }).should.be.fulfilled;
+            await this.interactor.setTokenOnProxy(this.token.address, { from: owner });
         });
 
         it('only owner can set token on proxy', async function() {
             await this.token.transferOwnership(this.interactor.address, { from: owner });
             await this.interactor.claimTokenOwnership({ from: owner });
-            await this.interactor.setTokenOnProxy(this.token.address, { from: admin1 }).should.be.rejected;
-            await this.interactor.setTokenOnProxy(this.token.address, { from: admin2 }).should.be.rejected;
-            await this.interactor.setTokenOnProxy(this.token.address, { from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.setTokenOnProxy(this.token.address, { from: admin1 }));
+            await shouldFail.reverting(this.interactor.setTokenOnProxy(this.token.address, { from: admin2 }));
+            await this.interactor.setTokenOnProxy(this.token.address, { from: owner });
         });
     });
 
@@ -241,18 +238,18 @@ contract('AdminInteractor Tests', function(accounts) {
 
         it('proxy should be owned by interactor', async function() {
             await this.proxy.transferOwnership(this.interactor.address, { from: owner });
-            await this.interactor.setProxyOnToken(this.proxy.address, { from: owner }).should.be.rejected;
+            await shouldFail.reverting(this.interactor.setProxyOnToken(this.proxy.address, { from: owner }));
 
             await this.interactor.claimProxyOwnership({ from: owner });
-            await this.interactor.setProxyOnToken(this.proxy.address, { from: owner }).should.be.fulfilled;
+            await this.interactor.setProxyOnToken(this.proxy.address, { from: owner });
         });
 
         it('only owner can set proxy on token', async function() {
             await this.proxy.transferOwnership(this.interactor.address, { from: owner });
             await this.interactor.claimProxyOwnership({ from: owner });
-            await this.interactor.setProxyOnToken(this.proxy.address, { from: admin1 }).should.be.rejected;
-            await this.interactor.setProxyOnToken(this.proxy.address, { from: admin2 }).should.be.rejected;
-            await this.interactor.setProxyOnToken(this.proxy.address, { from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.setProxyOnToken(this.proxy.address, { from: admin1 }));
+            await shouldFail.reverting(this.interactor.setProxyOnToken(this.proxy.address, { from: admin2 }));
+            await this.interactor.setProxyOnToken(this.proxy.address, { from: owner });
         });
     });
 
@@ -275,13 +272,13 @@ contract('AdminInteractor Tests', function(accounts) {
         });
 
         it('only owner can transfer ownership for interactor', async function() {
-            await this.interactor.transferOwnedContract(this.proxy.address, rest[0], { from: rest[0] }).should.be.rejected;
-            await this.interactor.transferOwnedContract(this.proxy.address, rest[0], { from: admin1 }).should.be.rejected;
-            await this.interactor.transferOwnedContract(this.proxy.address, rest[0], { from: admin2 }).should.be.rejected;
-            await this.interactor.transferOwnedContract(this.proxy.address, rest[0], { from: owner }).should.be.fulfilled;
+            await shouldFail.reverting(this.interactor.transferOwnedContract(this.proxy.address, rest[0], { from: rest[0] }));
+            await shouldFail.reverting(this.interactor.transferOwnedContract(this.proxy.address, rest[0], { from: admin1 }));
+            await shouldFail.reverting(this.interactor.transferOwnedContract(this.proxy.address, rest[0], { from: admin2 }));
+            await this.interactor.transferOwnedContract(this.proxy.address, rest[0], { from: owner });
 
             // new owner can now claim ownership
-            await this.proxy.claimOwnership({ from: rest[0] }).should.be.fulfilled;
+            await this.proxy.claimOwnership({ from: rest[0] });
 
             // new owner should be reflected after claim
             const newOwner = await this.proxy.owner();
@@ -290,7 +287,7 @@ contract('AdminInteractor Tests', function(accounts) {
 
         it('can only transfer ownership for interactor owned contracts', async function() {
             const anotherToken = await BaseToken.new('BaseToken', 'BT', 18, { from: owner });
-            await this.interactor.transferOwnedContract(anotherToken.address, rest[0], { from: owner }).should.be.rejected;
+            await shouldFail.reverting(this.interactor.transferOwnedContract(anotherToken.address, rest[0], { from: owner }));
         });
     });
 
@@ -322,27 +319,27 @@ contract('AdminInteractor Tests', function(accounts) {
             await this.allowanceModule.transferOwnership(this.token.address, { from: owner });   
             await this.registryModule.transferOwnership(this.token.address, { from: owner });   
 
-            await this.interactor.setBalanceModule(this.balanceModule.address, { from: rest[0] }).should.be.rejected;
-            await this.interactor.setAllowanceModule(this.allowanceModule.address, { from: rest[0] }).should.be.rejected;
-            await this.interactor.setRegistryModule(this.registryModule.address, { from: rest[0] }).should.be.rejected;
+            await shouldFail.reverting(this.interactor.setBalanceModule(this.balanceModule.address, { from: rest[0] }));
+            await shouldFail.reverting(this.interactor.setAllowanceModule(this.allowanceModule.address, { from: rest[0] }));
+            await shouldFail.reverting(this.interactor.setRegistryModule(this.registryModule.address, { from: rest[0] }));
 
-            await this.interactor.setBalanceModule(this.balanceModule.address, { from: owner }).should.be.fulfilled;
-            await this.interactor.setAllowanceModule(this.allowanceModule.address, { from: owner }).should.be.fulfilled;
-            await this.interactor.setRegistryModule(this.registryModule.address, { from: owner }).should.be.fulfilled;
+            await this.interactor.setBalanceModule(this.balanceModule.address, { from: owner });
+            await this.interactor.setAllowanceModule(this.allowanceModule.address, { from: owner });
+            await this.interactor.setRegistryModule(this.registryModule.address, { from: owner });
         });
 
         it('module ownership needs to be transferred to token first', async function() {
-            await this.interactor.setBalanceModule(this.balanceModule.address, { from: owner }).should.be.rejected;
-            await this.interactor.setAllowanceModule(this.allowanceModule.address, { from: owner }).should.be.rejected;
-            await this.interactor.setRegistryModule(this.registryModule.address, { from: owner }).should.be.rejected;
+            await shouldFail.reverting(this.interactor.setBalanceModule(this.balanceModule.address, { from: owner }));
+            await shouldFail.reverting(this.interactor.setAllowanceModule(this.allowanceModule.address, { from: owner }));
+            await shouldFail.reverting(this.interactor.setRegistryModule(this.registryModule.address, { from: owner }));
 
             await this.balanceModule.transferOwnership(this.token.address, { from: owner });   
             await this.allowanceModule.transferOwnership(this.token.address, { from: owner });   
             await this.registryModule.transferOwnership(this.token.address, { from: owner });   
 
-            await this.interactor.setBalanceModule(this.balanceModule.address, { from: owner }).should.be.fulfilled;
-            await this.interactor.setAllowanceModule(this.allowanceModule.address, { from: owner }).should.be.fulfilled;
-            await this.interactor.setRegistryModule(this.registryModule.address, { from: owner }).should.be.fulfilled;
+            await this.interactor.setBalanceModule(this.balanceModule.address, { from: owner });
+            await this.interactor.setAllowanceModule(this.allowanceModule.address, { from: owner });
+            await this.interactor.setRegistryModule(this.registryModule.address, { from: owner });
         });
     });
 });
