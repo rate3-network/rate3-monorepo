6cd462c91f7eaa709ec9e66167198b3e81b9a3ee
Waihon Chee <imwaihon@gmail.com>
2019-02-13 17:54:19 +0800

    IOST Conversion Receiver initial draft

--- packages/ethereum-contracts/contracts/bridge/IOSTConversionReceiver.sol
+++ packages/ethereum-contracts/contracts/bridge/IOSTConversionReceiver.sol
@@ -1,17 +1,34 @@
 pragma solidity 0.4.24;
 
 import "../lib/ownership/Claimable.sol";
+import "../lib/lifecycle/Pausable.sol";
+import "../lib/math/SafeMath.sol";
 import "../tokenization/interfaces/ERC20.sol";
 
-contract IOSTConversionReceiver is Claimable {
+contract IOSTConversionReceiver is Claimable, Pausable {
+    using SafeMath for uint256;
 
     ERC20 public token;
+    ERC20 public discountToken;
+    uint256 public discountThreshold;
+    uint256 public conversionFeeBasisPoints;
+    uint256 public unlockFeeBasisPoints;
+    uint256 public discountedConversionFeeBasisPoints;
+    uint256 public discountedUnlockFeeBasisPoints;
+    uint256 public minimumConversionAmount;
+    address public feeCollectionWallet;
+    address public coldStorageWallet;
     Conversion[] public conversions;
+    uint256 public totalLockedTokens;
+    uint256 public fees;
 
     struct Conversion {
         address ethAddress;
         string iostAccount;
         uint256 amount;
+        uint256 fee;
+        uint256 netAmount;
+        bool discounted;
         States state;
     }
 
@@ -22,26 +39,115 @@ contract IOSTConversionReceiver is Claimable {
         REJECTED
     }
 
-    event ConversionRequested(uint256 indexID, address indexed ethAddress, string iostAccount, uint256 amount, uint256 requestTimestamp);
-    event ConversionRejected(uint256 indexID, address indexed ethAddress, string iostAccount, uint256 amount, uint256 rejectTimestamp);
-    event ConversionAccepted(uint256 indexID, address indexed ethAddress, string iostAccount, uint256 amount, uint256 acceptTimestamp);
-    event ConversionUnlocked(
+    event ConversionRequested(
+        uint256 indexID,
         address indexed ethAddress,
         string iostAccount,
         uint256 amount,
-        uint256 unlockTimestamp,
-        string iostMirrorTransactionHash
+        uint256 fee,
+        uint256 netAmount,
+        bool discounted,
+        uint256 requestTimestamp
     );
-    event SentTokensToColdStorage(address coldStorageAddress, uint256 amount);
+    event ConversionRejected(
+        uint256 indexID,
+        address indexed ethAddress,
+        string iostAccount,
+        uint256 amount,
+        uint256 fee,
+        uint256 netAmount,
+        bool discounted,
+        uint256 rejectTimestamp
+    );
+    event ConversionAccepted(
+        uint256 indexID,
+        address indexed ethAddress,
+        string iostAccount,
+        uint256 amount,
+        uint256 fee,
+        uint256 netAmount,
+        bool discounted,
+        uint256 acceptTimestamp
+    );
+    event ConversionUnlocked(
+        address indexed ethAddress,
+        string iostAccount,
+        string iostMirrorTransactionHash,
+        uint256 amount,
+        uint256 fee,
+        uint256 netAmount,
+        bool discounted,
+        uint256 unlockTimestamp
+    );
+    event CollectedFees(address feeCollectionWallet, uint256 amount, uint256 collectionTimestamp);
+    event SentTokensToColdStorage(address coldStorageAddress, uint256 amount, uint256 storageTimestamp);
     
-
     modifier onlyOpenConversions(uint256 _index) {
         require(conversions[_index].state == States.OPEN, "Conversion should be open");
         _;
     }
 
-    constructor(ERC20 _token) public {
+    constructor(
+        ERC20 _token,
+        ERC20 _discountToken,
+        uint256 _discountThreshold,
+        uint256 _conversionFeeBasisPoints,
+        uint256 _unlockFeeBasisPoints,
+        uint256 _discountedConversionFeeBasisPoints,
+        uint256 _discountedUnlockFeeBasisPoints,
+        uint256 _minimumConversionAmount,
+        address _feeCollectionWallet,
+        address _coldStorageWallet
+    ) public {
         token = _token;
+        discountToken = _discountToken;
+        discountThreshold = _discountThreshold;
+        conversionFeeBasisPoints = _conversionFeeBasisPoints;
+        unlockFeeBasisPoints = _unlockFeeBasisPoints;
+        discountedConversionFeeBasisPoints = _discountedConversionFeeBasisPoints;
+        discountedUnlockFeeBasisPoints = _discountedUnlockFeeBasisPoints;
+        minimumConversionAmount = _minimumConversionAmount;
+        feeCollectionWallet = _feeCollectionWallet;
+        coldStorageWallet = _coldStorageWallet;
+    }
+
+    // Owner functions, only when contract is paused
+    function setDiscountToken(ERC20 _discountToken) public onlyOwner whenPaused {
+        discountToken = _discountToken;
+    }
+
+    function setDiscountThreshold(uint256 _threshold) public onlyOwner whenPaused {
+        discountThreshold = _threshold;
+    }
+
+    function setConversionFeeBasisPoints(uint256 _bp) public onlyOwner whenPaused {
+        conversionFeeBasisPoints = _bp;
+    }
+
+    function setUnlockFeeBasisPoints(uint256 _bp) public onlyOwner whenPaused {
+        unlockFeeBasisPoints = _bp;
+    }
+
+    function setDiscountedConversionFeeBasisPoints(uint256 _bp) public onlyOwner whenPaused {
+        require(_bp <= conversionFeeBasisPoints, "Discount cannot be higher than non-discounted rate");
+        discountedConversionFeeBasisPoints = _bp;
+    }
+
+    function setDiscountedUnlockFeeBasisPoints(uint256 _bp) public onlyOwner whenPaused {
+        require(_bp <= unlockFeeBasisPoints, "Discount cannot be higher than non-discounted rate");
+        discountedUnlockFeeBasisPoints = _bp;
+    }
+
+    function setMinimumConversionAmount(uint256 _amount) public onlyOwner whenPaused {
+        minimumConversionAmount = _amount;
+    }
+
+    function setFeeCollectionWallet(address _wallet) public onlyOwner whenPaused {
+        feeCollectionWallet = _wallet;
+    }
+
+    function setColdStorageWallet(address _wallet) public onlyOwner whenPaused {
+        coldStorageWallet = _wallet;
     }
 
     function requestConversion(
@@ -49,29 +155,78 @@ contract IOSTConversionReceiver is Claimable {
         string _iostAccount
     )
         public
+        whenNotPaused
     {
+        require(_amount >= minimumConversionAmount, "Should be above minimum conversion amount");
+        require(_amount <= token.allowance(msg.sender, address(this)), "Allowance should be set");
+        
         uint256 index = conversions.length;
 
-        require(_amount <= token.allowance(msg.sender, address(this)), "Allowance should be set");
+        uint256 fee = 0;
+        bool discounted = (discountToken.balanceOf(msg.sender) >= discountThreshold);
+        // Check whether discounted fee or normal fee.
+        if (discounted) {
+            fee = (_amount.mul(discountedConversionFeeBasisPoints)).div(10000);
+        } else {
+            fee = (_amount.mul(conversionFeeBasisPoints)).div(10000);
+        }
+
+        uint256 netAmount = _amount.sub(fee);
+
+        conversions.push(
+            Conversion(
+                msg.sender,
+                _iostAccount,
+                _amount,
+                fee,
+                netAmount,
+                discounted,
+                States.OPEN
+            )
+        );
+
+        // Transfer total amount to conversion contract.
         require(token.transferFrom(msg.sender, address(this), _amount), "Token transfer failed");
 
-        conversions.push(Conversion(msg.sender, _iostAccount, _amount, States.OPEN));
-
-        emit ConversionRequested(index, msg.sender, _iostAccount, _amount, block.timestamp);
+        emit ConversionRequested(index, msg.sender, _iostAccount, _amount, fee, netAmount, discounted, block.timestamp);
     }
 
-    function rejectConversion(uint256 _index) public onlyOwner onlyOpenConversions(_index) {
+    function rejectConversion(uint256 _index) public onlyOwner whenNotPaused onlyOpenConversions(_index) {
         Conversion storage conversion = conversions[_index];
         require(token.transfer(conversion.ethAddress, conversion.amount), "Token transfer failed");
         conversion.state = States.REJECTED;
 
-        emit ConversionRejected(_index, conversion.ethAddress, conversion.iostAccount, conversion.amount, block.timestamp);
+        emit ConversionRejected(
+            _index,
+            conversion.ethAddress,
+            conversion.iostAccount,
+            conversion.amount,
+            conversion.fee,
+            conversion.netAmount,
+            conversion.discounted,
+            block.timestamp
+        );
     }
 
-    function acceptConversion(uint256 _index) public onlyOwner onlyOpenConversions(_index) {
+    function acceptConversion(uint256 _index) public onlyOwner whenNotPaused onlyOpenConversions(_index) {
         Conversion storage conversion = conversions[_index];
         conversion.state = States.ACCEPTED;
-        emit ConversionAccepted(_index, conversion.ethAddress, conversion.iostAccount, conversion.amount, block.timestamp);
+
+        // Add net amount to locked tokens.
+        totalLockedTokens = totalLockedTokens.add(conversion.netAmount);
+        // Add fee to total fees collected.
+        fees = fees.add(conversion.fee);
+
+        emit ConversionAccepted(
+            _index,
+            conversion.ethAddress,
+            conversion.iostAccount,
+            conversion.amount,
+            conversion.fee,
+            conversion.netAmount,
+            conversion.discounted,
+            block.timestamp
+        );
     }
 
     function unlockConversion(
@@ -82,15 +237,50 @@ contract IOSTConversionReceiver is Claimable {
     )
         public
         onlyOwner
+        whenNotPaused
     {
         require(_amount <= token.balanceOf(address(this)), "Not enough tokens to convert");
-        require(token.transfer(_ethAddress, _amount), "Token transfer failed");
 
-        emit ConversionUnlocked(_ethAddress, _iostAccount, _amount, block.timestamp, _iostMirrorTransactionHash);
+        uint256 fee = 0;
+        bool discounted = (discountToken.balanceOf(_ethAddress) >= discountThreshold);
+        // Check whether discounted fee or normal fee.
+        if (discounted) {
+            fee = (_amount.mul(discountedUnlockFeeBasisPoints)).div(10000);
+        } else {
+            fee = (_amount.mul(unlockFeeBasisPoints)).div(10000);
+        }
+
+        uint256 netAmount = _amount.sub(fee);
+        // Remove total amount from locked tokens.
+        totalLockedTokens = totalLockedTokens.sub(_amount);
+        // Add fee to total fees collected.
+        fees = fees.add(fee);
+
+        // Transfer (amount - fees) to destination address.
+        require(token.transfer(_ethAddress, netAmount), "Token transfer failed");
+
+        emit ConversionUnlocked(
+            _ethAddress,
+            _iostAccount,
+            _iostMirrorTransactionHash,
+            _amount,
+            fee,
+            netAmount,
+            discounted,
+            block.timestamp
+        );
     }
 
-    function sendTokensToColdStorage(address _coldStorageAddress, uint256 _amount) public onlyOwner {
-        require(token.transfer(_coldStorageAddress, _amount), "Token transfer failed");
-        emit SentTokensToColdStorage(_coldStorageAddress, _amount);
+    function collectFees() public onlyOwner {
+        uint256 amount = fees;
+        // Clear fees first *important*;
+        fees = 0;
+        require(token.transfer(feeCollectionWallet, amount), "Token transfer failed");
+        emit CollectedFees(feeCollectionWallet, amount, block.timestamp);
+    }
+
+    function sendTokensToColdStorage(uint256 _amount) public onlyOwner whenPaused {
+        require(token.transfer(coldStorageWallet, _amount), "Token transfer failed");
+        emit SentTokensToColdStorage(coldStorageWallet, _amount, block.timestamp);
     }
 }
--- packages/ethereum-contracts/migrations/5_deploy_converter.js
+++ packages/ethereum-contracts/migrations/5_deploy_converter.js
@@ -1,5 +1,3 @@
-// const BaseInteractor = artifacts.require('./tokenization/interactors/BaseInteractor.sol');
-// const BaseProxy = artifacts.require('./tokenization/BaseProxy.sol');
 const BaseToken = artifacts.require('./tokenization/ModularToken.sol');
 const BalanceModule = artifacts.require('./tokenization/modules/BalanceModule.sol');
 const AllowanceModule = artifacts.require('./tokenization/modules/AllowanceModule.sol');
@@ -23,9 +21,6 @@ module.exports = function deployment(deployer, network, accounts) {
         console.log('\nDeploying BaseToken');
         const token = await BaseToken.new('Test SGD', 'TESTSGD', 18, { from: owner });
 
-        // console.log('\nDeploying BaseProxy');
-        // const proxy = await BaseProxy.new(token.address, { from: owner });
-
         console.log('\nTransfer ownership of BalanceModule to Token');
         await balance.transferOwnership(token.address, { from: owner });
 
@@ -44,42 +39,61 @@ module.exports = function deployment(deployer, network, accounts) {
         console.log('\nSetting token\'s RegistryModule');
         await token.setRegistryModule(registry.address, { from: owner });
 
-        // console.log('\nDeploying BaseInteractor');
-        // const interactor = await BaseInteractor.new(token.address, proxy.address, { from: owner });
+        console.log('\nDeploying BalanceModule');
+        const balance2 = await BalanceModule.new({ from: owner });
 
-        // console.log('\nTransferring ownership of token to operations interactor');
-        // await token.transferOwnership(interactor.address, { from: owner });
-        // await interactor.setToken(token.address, { from: owner });
-        // await interactor.claimTokenOwnership({ from: owner });
+        console.log('\nDeploying AllowanceModule');
+        const allowance2 = await AllowanceModule.new({ from: owner });
 
-        // console.log('\nTransferring ownership of proxy to operations interactor');
-        // await proxy.transferOwnership(interactor.address, { from: owner });
-        // await interactor.setProxy(proxy.address, { from: owner });
-        // await interactor.claimProxyOwnership({ from: owner });
+        console.log('\nDeploying RegistryModule');
+        const registry2 = await RegistryModule.new({ from: owner });
 
-        // console.log('\nSet token on proxy');
-        // await interactor.setTokenOnProxy(token.address, { from: owner });
+        console.log('\nDeploying BaseToken');
+        const token2 = await BaseToken.new('Rate3', 'RTE', 18, { from: owner });
 
-        // console.log('\nSet proxy on token');
-        // await interactor.setProxyOnToken(proxy.address, { from: owner });
+        console.log('\nTransfer ownership of BalanceModule to Token');
+        await balance2.transferOwnership(token2.address, { from: owner });
 
-        // console.log('\nSetting admin 1 (approval) of BaseInteractor');
-        // await interactor.setFirstAdmin(admin1, { from: owner });
+        console.log('\nTransfer ownership of AllowanceModule to Token');
+        await allowance2.transferOwnership(token2.address, { from: owner });
 
-        // console.log('\nSetting admin 2 (finalize) of BaseInteractor');
-        // await interactor.setSecondAdmin(admin2, { from: owner });
+        console.log('\nTransfer ownership of RegistryModule to Token');
+        await registry2.transferOwnership(token2.address, { from: owner });
+
+        console.log('\nSetting token\'s BalanceModule');
+        await token2.setBalanceModule(balance2.address, { from: owner });
+
+        console.log('\nSetting token\'s AllowanceModule');
+        await token2.setAllowanceModule(allowance2.address, { from: owner });
+
+        console.log('\nSetting token\'s RegistryModule');
+        await token2.setRegistryModule(registry2.address, { from: owner });
 
         console.log('\nSetting converter of BaseToken');
-        const converter = await ConversionReceiver.new(token.address, { from: owner });
+        const converter = await ConversionReceiver.new(
+            token.address,
+            token2.address,
+            new web3.utils.BN('1000000000000000000000'), // 1000 RTE tokens
+            0,
+            10,
+            0,
+            5,
+            new web3.utils.BN('1000000000000000000'), // 1 token
+            admin1,
+            admin2,
+            { from: owner }
+        );
 
         console.log('\n===== Addresses ======');
-        console.log('AllowanceModule:', allowance.address);
-        console.log('BalanceModule:  ', balance.address);
-        console.log('RegistryModule: ', registry.address);
-        console.log('Token:          ', token.address);
-        // console.log('Proxy:          ', proxy.address);
-        // console.log('Interactor:     ', interactor.address);
-        console.log('Converter:      ', converter.address);
+        console.log('AllowanceModule:    ', allowance.address);
+        console.log('BalanceModule:      ', balance.address);
+        console.log('RegistryModule:     ', registry.address);
+        console.log('Token:              ', token.address);
+        console.log('RTE AllowanceModule:', allowance2.address);
+        console.log('RTE BalanceModule:  ', balance2.address);
+        console.log('RTE RegistryModule: ', registry2.address);
+        console.log('RTE Token:          ', token2.address);
+        console.log('Converter:          ', converter.address);
         console.log('======================\n');
 
         console.log('\n===== Admins ======');
